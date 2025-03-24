7855eac2fb742fa6677a94e0a66cd62dc92a7ae2
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-03-23 23:19:26 -0500

    style: update button styles

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
 // Components
-import { AccessibilityIcon } from "./icons";
-
-import "../styles.css";
+import { AccessibilityIcon } from "./icons.jsx";
 
 export default function Button({ className = "", ...props }) {
     return (
         <button
             className={
-                "fixed bottom-[5rem] right-[6rem] w-[50px] h-[50px] bg-white border-[3px] border-sky-950 rounded-full flex items-center justify-center active:scale-95 duration-300 cursor-pointer" +
+                "fixed bottom-[5rem] right-[6rem] w-[50px] h-[50px] bg-white border-[3px] border-primary rounded-full flex items-center justify-center active:scale-95 ease-out duration-300 cursor-pointer" +
                 className
             }
             {...props}
